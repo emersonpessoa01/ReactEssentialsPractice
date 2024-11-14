4670244ff202e4634e931fa71b97d86983e42b58
emersonpessoa01 <eps011108@gmail.com>
2024-11-14 14:13:18 -0300

    first commit

---
+++ src/pages/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Logo from "./assets/image/logo-react-practice.png";
+import "../styles/reset.css";
+import "../styles/global.css";
+import "../styles/global.css";
+function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <div>
+        <img src={Logo} alt="Logo" />
+      </div>
+    </>
+  );
+}
+
+export default App;
