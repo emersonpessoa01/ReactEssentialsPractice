7f3778245c4b70def6c12d6b0e337e2b87f502ee
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2024-11-15 14:06:33 -0300

    feat: updated App :sparkles:

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
-import Dashboard from "./pages/Dashboard";
-import Footer from "./pages/Footer";
-import Header from "./pages/Header";
-import Sidebar from "./pages/Sidebar";
-function App() {
+// src/App.js
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
+// Importação dos componentes
+
+
+const App = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <div className="main-content">
-      <Header />
-      <Sidebar />
-      <Dashboard />
-      <Footer />
+    <div className="dashboard">
+      <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+      <div className="main-content">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/counter" component={Counter} />
+            <Route path="/background-color-changer" component={BackgroundColorChanger} />
+            <Route path="/todo-list" component={TodoList} />
+            <Route path="/timer" component={Timer} />
+            <Route path="/filtered-list" component={FilteredList} />
+            <Route path="/simple-form" component={SimpleForm} />
+            <Route path="/data-fetcher" component={DataFetcher} />
+            <Route path="/image-gallery" component={ImageGallery} />
+            <Route path="/countdown-timer" component={CountdownTimer} />
+            <Route path="/tab-navigation" component={TabNavigation} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
